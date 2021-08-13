bb8487127ecbc9ce1334bf5db0b995c9f03798cb
Matustylee <matiasmorales1091@gmail.com>
2021-08-12 23:26:32 -0300

    cambios en login
    probando funciones de ingreso

--- js/login.js
+++ js/login.js
@@ -1,6 +1,12 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
-});
+function ingreso(){
+    if(document.getElementsByName("usuario").value !== "" && 
+    document.getElementsByName("pass").value !=="" ){
+    window.location.replace("home.html")}
+    else{
+        alert("Debe llenar los campos");
+    }
+}
