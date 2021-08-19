1c09a82d8747fbb8647ef1c0ac939e4b75cb171f
Matustylee <matiasmorales1091@gmail.com>
2021-08-19 04:22:57 -0300

    cambios prueba

--- js/login.js
+++ js/login.js
@@ -11,7 +11,7 @@ function onSignIn(googleUser) {
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
     var id_token = googleUser.getAuthResponse().id_token;
-    console.log("id_token");
+    console.log(id_token);
   }
 
   function signOut() {
