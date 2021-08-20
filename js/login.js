e4bad145e03b0057fa51ba7a50e3b5c76c458df8
Matustylee <matiasmorales1091@gmail.com>
2021-08-20 08:25:14 -0300

    summary

--- js/login.js
+++ js/login.js
@@ -17,9 +17,11 @@ function onSignIn(googleUser) {
   }
 
   function signOut() {
+    var url = "https://matustylee.github.io/jap_repository/index.html"
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
+      window.location.href = url;
     });
   }
 
