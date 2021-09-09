e2f5a848fd38b62bb41dce0b8670530dead1df0b
Matias Morales <matiasmorales1091@gmail.com>
2021-09-09 18:56:56 -0300

    update product.info

--- js/product-info.js
+++ js/product-info.js
@@ -1,112 +1,95 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
 var products = {};
 var comments = [];
 var productsList = [];
-const ORDER_BY_DATATIME = "Fecha.";  /*probando para ordenar por fecha comentarios*/
+const ORDER_BY_DATATIME =
+  "Fecha."; /*probando para ordenar por fecha comentarios*/
 
 /* Escucha carga descripcion y carusel*/
-document.addEventListener("DOMContentLoaded", function(e){
-        getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
-            if (resultObj.status === "ok")
-            {
-                products = resultObj.data;
-                let productsNameHTML  = document.getElementById("productName");                             
-                let productsDescriptionHTML = document.getElementById("productsDescription");
-                let soldCountHTML = document.getElementById("soldCount");                
-                let productCategoryHTML = document.getElementById("productCategory");               
-                
-                productsNameHTML.innerHTML = `${products.name} : <strong>Precio ${products.currency} ${products.cost}</strong>`;                         
-                productsDescriptionHTML.innerHTML = products.description;
-                soldCountHTML.innerHTML = products.soldCount+" "+"Articulos";                
-                productCategoryHTML.innerHTML = `<a href= "../jap_repository/products.html">${products.category}</a>`; 
-                Imgcarusel(products.images);            
-                
-            }                           
-            
-        });
-    });
-    
-  //Agrego imagenes a carrusel
- function Imgcarusel(array){
-    
-    let HTMLcontent = "";
-    for (let i = 0; i < array.length; i++) {
-      let product = array[i];
+document.addEventListener("DOMContentLoaded", function (e) {
+  getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
+    if (resultObj.status === "ok") {
+      products = resultObj.data;
+      let productsNameHTML = document.getElementById("productName");
+      let productsDescriptionHTML = document.getElementById(
+        "productsDescription"
+      );
+      let soldCountHTML = document.getElementById("soldCount");
+      let productCategoryHTML = document.getElementById("productCategory");
 
-      HTMLcontent += `<div class="carousel-item">   
+      productsNameHTML.innerHTML = `${products.name} : <strong>Precio ${products.currency} ${products.cost}</strong>`;
+      productsDescriptionHTML.innerHTML = products.description;
+      soldCountHTML.innerHTML = products.soldCount + " " + "Articulos";
+      productCategoryHTML.innerHTML = `<a href= "../jap_repository/products.html">${products.category}</a>`;
+      Imgcarusel(products.images);
+    }
+  });
+  //escucha carga productos relacionados
+  getJSONData(PRODUCTS_URL).then(function (resultObj) {
+    if (resultObj.status === "ok") {
+      productsList = resultObj.data;
+      //console.log(productsList)
+      let htmlContentToAppend = "";
+      let relacionados = products["relatedProducts"];
+      for (let i = 0; i < relacionados.length; i++) {
+        let related = relacionados[i];
+        let producto = productsList[related];
+
+        htmlContentToAppend += `    
+                         <div class="col-sm-6">               
+                         <div class="card">
+                         <a class="card-header" href= "../jap_repository/products.html" >${producto.name}</a> 
+                         <div class="card-body">
+                         <img class="img-fluid img-thumbnail" src="${producto.imgSrc}" alt="">
+                         <p class="card-text class-muted">${producto.description}</p>
+                         <h3 class="text-center"><strong>${producto.currency}${producto.cost}</strong></h3>
+                         </div>
+                         </div>
+                         </div> `;
+      }
+      document.getElementById("relatedProducts").innerHTML =
+        htmlContentToAppend;
+    }
+  });
+  /* escucha carga comentarios*/
+  getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj) {
+    if (resultObj.status === "ok") {
+      comments = resultObj.data;
+      commentsPost();
+    }
+  });
+});
+
+//Agrego imagenes a carrusel
+function Imgcarusel(array) {
+  let HTMLcontent = "";
+  for (let i = 0; i < array.length; i++) {
+    let product = array[i];
+
+    HTMLcontent += `<div class="carousel-item">   
     <img src="${product}"  class="d-block w-100" alt="">
     </div>                
-    `
-    }
-    document.getElementById("productImagesGallery").innerHTML = HTMLcontent;
-    // funcion add class active carousel
-    $(document).ready(function () {
-        $('#myCarousel').find('.carousel-item').first().addClass('active');
-      });
+    `;
+  }
+  document.getElementById("productImagesGallery").innerHTML = HTMLcontent;
+  // funcion add class active carousel
+  $(document).ready(function () {
+    $("#myCarousel").find(".carousel-item").first().addClass("active");
+  });
+}
 
- }
- 
 /* Intervalo carrusel*/
 
-$('.carousel').carousel({
-  interval: 3000
-})
-
-/* escucha carga comentarios*/
-document.addEventListener("DOMContentLoaded", function(e){
-  getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
-      if (resultObj.status === "ok")
-      {
-          comments = resultObj.data;   
-          commentsPost();
-      }})
-      
-      
-    });
-   //escucha carga productos relacionados
-document.addEventListener("DOMContentLoaded", function(e){
-   getJSONData(PRODUCTS_URL).then(function(resultObj){
-        if (resultObj.status === "ok")
-        {
-            productsList = resultObj.data; 
-            //console.log(productsList)
-            let htmlContentToAppend = "";
-                
-              for(let i = 0; i < productsList.length; i++){
-                 let related = productsList[i];
-                   if ( i == products["relatedProducts"][0] || i == products.relatedProducts[1] ) {
-                      //console.log(related);
-                      htmlContentToAppend += `    
-                       <div class="col-sm-6">               
-                       <div class="card">
-                       <a class="card-header" href= "../jap_repository/products.html" >${related.name}</a> 
-                       <div class="card-body">
-                       <img class="img-fluid img-thumbnail" src="${related.imgSrc}" alt="">
-                       <p class="card-text class-muted">${related.description}</p>
-                       <h3 class="text-center"><strong>${related.currency}${related.cost}</strong></h3>
-                       </div>
-                       </div>
-                       </div> `
-                    }
-                    document.getElementById("relatedProducts").innerHTML = htmlContentToAppend;
-                }
-            }   
-            })
-          });
-
-    
+$(".carousel").carousel({
+  interval: 3000,
+});
 
 /*funcion cargar Comentarios*/
-function commentsPost(){
+function commentsPost() {
+  let HTMLcontent = "";
+  for (let i = 0; i < comments.length; i++) {
+    let comment = comments[i];
 
-   let HTMLcontent = "";
-   for (let i = 0; i < comments.length; i++) {
-     let comment = comments[i];
-       
-     
-     HTMLcontent += ` <li class="media">
+    HTMLcontent += ` <li class="media">
      <a href="#" class="float-left">
          <img src="img/face_icon.jpg" alt="" class="img-circle">
      </a>
@@ -119,35 +102,32 @@ function commentsPost(){
              ${comment.description}
          </p>                      
          <span class="stars">${comment.score}</span>            
-     </div> </li>`
-        
-   }   
-   document.getElementById("commentConteiner").innerHTML = HTMLcontent;  
-   
-   $('.stars').stars();
+     </div> </li>`;
+  }
+  document.getElementById("commentConteiner").innerHTML = HTMLcontent;
+
+  $(".stars").stars();
 }
 
 /* Comentar Desafio*/
-function comentar(){
+function comentar() {
   let texto = document.getElementById("textAreaControl").value;
   let user = JSON.parse(localStorage.getItem("userName"));
   let fecha = new Date();
-  let result = fecha.toLocaleDateString("es-ES", { // you can use undefined as first argument
+  let result = fecha.toLocaleDateString("es-ES", {
     day: "2-digit",
     month: "2-digit",
     year: "numeric",
   });
   //toISOString().split('T')[0]; Para fecha yyyy/mm/dd
-  //let dia = fecha.getDay();
-  //let mes = fecha.getMonth();
-  //let año = fecha.getFullYear();
-  let hora=fecha.getHours(); //hora actual
-  let minuto=fecha.getMinutes(); //minuto actual
-  let segundo=fecha.getSeconds(); //segundo actual   
-  let score = document.querySelector(`input[type="radio"][name=rating]:checked`).value
- 
-  
-  document.getElementById("commentConteiner").innerHTML +=  ` <li class="media">
+  let hora = fecha.getHours(); //hora actual
+  let minuto = fecha.getMinutes(); //minuto actual
+  let segundo = fecha.getSeconds(); //segundo actual
+  let score = document.querySelector(
+    `input[type="radio"][name=rating]:checked`
+  ).value;
+
+  document.getElementById("commentConteiner").innerHTML += ` <li class="media">
   <a href="#" class="float-left">
       <img src="img/face_icon.jpg" alt="" class="img-circle">
   </a>
@@ -160,27 +140,14 @@ function comentar(){
           ${texto}
       </p>      
       <span class="stars">${score}</span>
-  </div> </li>`
-  
-  $('.stars').stars();
-  
+  </div> </li>`;
+
+  $(".stars").stars();
 }
 
-
 /*funcion mostrar score en estrellas*/
-$.fn.stars = function() {
-  return this.each(function(i,e){$(e).html($('<span/>').width($(e).text()*16));});
+$.fn.stars = function () {
+  return this.each(function (i, e) {
+    $(e).html($("<span/>").width($(e).text() * 16));
+  });
 };
-
-
-
-       
-
-    
-
-
-       
-        
-            
-
-
