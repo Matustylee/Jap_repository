662f44a5957aebeed6ea5c002b3a41bd2b4e809a
Matias Morales <matiasmorales1091@gmail.com>
2021-09-08 22:25:40 -0300

    productinfo
    carggar productos relacionados
    etc

--- js/product-info.js
+++ js/product-info.js
@@ -3,7 +3,9 @@
 //elementos HTML presentes.
 var products = {};
 var comments = [];
-const ORDER_BY_DATATIME = "Fecha.";
+var productsList = [];
+
+const ORDER_BY_DATATIME = "Fecha.";  /*probando para ordenar por fecha comentarios*/
 
 /* Escucha carga descripcion y carusel*/
 document.addEventListener("DOMContentLoaded", function(e){
@@ -11,19 +13,17 @@ document.addEventListener("DOMContentLoaded", function(e){
             if (resultObj.status === "ok")
             {
                 products = resultObj.data;
-    
                 let productsNameHTML  = document.getElementById("productName");                             
                 let productsDescriptionHTML = document.getElementById("productsDescription");
                 let soldCountHTML = document.getElementById("soldCount");
-                let relatedProductsHTML = document.getElementById("relatedProducts");
+                //let relatedProductsHTML = document.getElementById("relatedProducts");
                 let gallery=document.getElementById("productImagesGallery");
-                let productCategoryHTML = document.getElementById("productCategory");
+                let productCategoryHTML = document.getElementById("productCategory");               
             
                 productsNameHTML.innerHTML = `${products.name} : <strong>Precio ${products.currency} ${products.cost}</strong>`;                         
                 productsDescriptionHTML.innerHTML = products.description;
-                soldCountHTML.innerHTML = products.soldCount+" "+"Articulos";
-                relatedProductsHTML.innerHTML = products.relatedProducts;
-                productCategoryHTML.innerHTML = products.category;                
+                soldCountHTML.innerHTML = products.soldCount+" "+"Articulos";                
+                productCategoryHTML.innerHTML = `<a href= "../jap_repository/products.html">${products.category}</a>`; 
                 gallery.innerHTML= `<div class="carousel-item active">
                 <img src="${products.images[0]}"  class="d-block w-100" alt="${products.name}">
                 </div>
@@ -45,27 +45,9 @@ document.addEventListener("DOMContentLoaded", function(e){
             
         });
     });
-    /*
-
-function carrusel(){
-  let gallery = "";
-  
-  for (let i = 1; i < products.length; i++) {
-    let imagenes = products[i];
-
-    gallery +=  `<div class="item">
-    <img src="${imagenes.images}"  alt="${imagenes.name}">
-    </div>`
     
-  }
 
-  document.getElementById("productImagesGallery").innerHTML = gallery;
-}
-carrusel();
-*/
-    
 /* Intervalo carrusel*/
-
 
 $('.carousel').carousel({
   interval: 3000
@@ -82,7 +64,38 @@ document.addEventListener("DOMContentLoaded", function(e){
       
       
     });
+   //escucha carga productos relacionados
+    document.addEventListener("DOMContentLoaded", function(e){
+        getJSONData(PRODUCTS_URL).then(function(resultObj){
+            if (resultObj.status === "ok")
+            {
+                productsList = resultObj.data; 
+                console.log(productsList)
+                let htmlContentToAppend = "";
+                
+                for(let i = 0; i < productsList.length; i++){
+                    let related = productsList[i];
+                    if ( i == products["relatedProducts"][0] || i == products.relatedProducts[1] ) {
+                        console.log(related);
+                        htmlContentToAppend += `    
+                                           <div class="col-sm-6">               
+                                             <div class="card">
+                                             <a class="card-header" href= "../jap_repository/products.html" >${related.name}</a> 
+                                             <div class="card-body">
+                                            <img class="img-fluid img-thumbnail" src="${related.imgSrc}" alt="">
+                                          <p class="card-text class-muted">${related.description}</p>
+                                         <h3 class="text-center"><strong>${related.currency}${related.cost}</strong></h3>
+                                        </div>
+                                         </div>
+                                         </div> `
+                    }
+                    document.getElementById("relatedProducts").innerHTML = htmlContentToAppend;
+                }
+            }   
+            })
+          });
 
+    
 
 /*Carga Comentarios*/
 function commentsPost(){
@@ -103,17 +116,13 @@ function commentsPost(){
          <strong class="text-success ml-2" >${comment.user}</strong>
          <p class="ml-1">
              ${comment.description}
-         </p>     
-                 
-         <span class="stars">${comment.score}</span>       
-    
-         
+         </p>                      
+         <span class="stars">${comment.score}</span>            
      </div> </li>`
         
-   }
+   }   
+   document.getElementById("commentConteiner").innerHTML = HTMLcontent;  
    
-   document.getElementById("commentConteiner").innerHTML = HTMLcontent;
-  
    $('.stars').stars();
 }
 
@@ -122,22 +131,28 @@ function comentar(){
   let texto = document.getElementById("textAreaControl").value;
   let user = JSON.parse(localStorage.getItem("userName"));
   let fecha = new Date();
-  let dia = fecha.getDay();
-  let mes = fecha.getMonth();
-  let año = fecha.getFullYear();
+  let result = fecha.toLocaleDateString("es-ES", { // you can use undefined as first argument
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  });
+  //toISOString().split('T')[0]; Para fecha yyyy/mm/dd
+  //let dia = fecha.getDay();
+  //let mes = fecha.getMonth();
+  //let año = fecha.getFullYear();
   let hora=fecha.getHours(); //hora actual
   let minuto=fecha.getMinutes(); //minuto actual
   let segundo=fecha.getSeconds(); //segundo actual   
   let score = document.querySelector(`input[type="radio"][name=rating]:checked`).value
  
- 
+  
   document.getElementById("commentConteiner").innerHTML +=  ` <li class="media">
   <a href="#" class="float-left">
       <img src="img/face_icon.jpg" alt="" class="img-circle">
   </a>
   <div class="media-body">
       <span class="text-muted float-right">
-          <p class="text-muted">${año}-${mes}-${dia} ${hora}:${minuto}:${segundo}</p>
+          <p class="text-muted">${result} ${hora}:${minuto}:${segundo}</p>
       </span>
       <strong class="text-success ml-2" >${user}</strong>
       <p class="ml-1">
@@ -158,9 +173,13 @@ $.fn.stars = function() {
 
 
 
+       
+
+    
 
 
-
-
+       
+        
+            
 
 
