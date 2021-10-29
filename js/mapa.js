58d9418b9fce7d467f208ea8fa65192709e6b308
Matias Morales <matiasmorales1091@gmail.com>
2021-10-29 09:26:44 -0300

    update mapa.js

--- js/mapa.js
+++ js/mapa.js
@@ -1,5 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
+
+var marker = L.marker([51.5, -0.09]).addTo(map)
+.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+.openPopup();
+
 var circle = L.circle([51.508, -0.11], {
     color: 'red',
     fillColor: '#f03',
@@ -12,13 +18,9 @@ var polygon = L.polygon([
     [51.503, -0.06],
     [51.51, -0.047]
 ]).addTo(mymap);
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
 
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
+})
