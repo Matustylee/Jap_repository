91814d11872e882d226335e34d92d0aea0cb7b62
Matias Morales <matiasmorales1091@gmail.com>
2021-11-17 11:10:41 -0300

    GeoMap

--- js/cart.js
+++ js/cart.js
@@ -164,12 +164,12 @@ $(function () {
 });
 
 /* funcion mensaje exitoso */
-function cargarMensaje(){
+function cargarMensaje(){ 
   let mensaje = cargarComentario.msg;
   Swal.fire({
   icon: 'success',
   title: mensaje
-})
+}) 
 };
 
 /* Escucha de input */
@@ -186,3 +186,42 @@ function removeCartItem(event) {
   localStorage.setItem('cantidadCarrito', JSON.stringify(cantidad));
   updateTotal();
 };
+
+
+
+
+
+// Disable form submissions if there are invalid fields
+(function() {
+  'use strict';
+  
+  window.addEventListener('load', function() {
+    // Get the forms we want to add validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    
+    Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+         
+            
+          
+          
+        }
+        form.classList.add('was-validated');
+        
+      }, false);
+      cargarMensaje()
+      
+      
+      
+    });
+    
+    
+    
+  }, false);
+ 
+  
+})();
--- js/mapa.js
+++ js/mapa.js
@@ -1,26 +1,48 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    var map = L.map('map').setView([-34.879625, -56.076622], 38);
 
-var marker = L.marker([51.5, -0.09]).addTo(map)
-.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-.openPopup();
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    
+    L.circle([-34.879625, -56.076622],{
+                color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 100
+     } ).addTo(map)
+         
+        .bindPopup('Nuestro Local Se encuentra Aquí')
+        .openPopup();
+    
 
-var circle = L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(mymap);
+        navigator.geolocation.getCurrentPosition(
+            function(position){
+                var latitude  = position.coords.latitude;
+                var longitude = position.coords.longitude;
+                console.log("Latitude : "+latitude+" Longitude : "+longitude);
+                L.marker([latitude, longitude]).addTo(map)
+               .bindPopup('Usted Se encuentra Aquí')
+               .openPopup();
+            },
+           
+        );     
+        
+ /*    var popup = L.popup();
 
-var polygon = L.polygon([
-    [51.509, -0.08],
-    [51.503, -0.06],
-    [51.51, -0.047]
-]).addTo(mymap);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+        function onMapClick(e) {
+            popup
+                .setLatLng(e.latlng)
+                .setContent("You clicked the map at " + e.latlng.toString())
+                .openOn(map);
+        }
+        
+        map.on('click', onMapClick);     
+    */
+        
 
 })
+
+
+
